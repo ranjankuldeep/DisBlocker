0aa6a982e4e02221b608bbbdd11d3957746cbfed
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-06-26 21:10:35 +0530

    feat: added signing the transaction

--- types.go/transaction.go
+++ types.go/transaction.go
@@ -3,10 +3,14 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/ranjankuldeep/DisBlocker/crypto"
 	"github.com/ranjankuldeep/DisBlocker/proto"
 	pb "google.golang.org/protobuf/proto"
 )
 
+func SignTransaction(pk *crypto.PrivateKey, tx *proto.Transaction) *crypto.Signature {
+	return pk.Sign(HashTransaction(tx))
+}
 func HashTransaction(tx *proto.Transaction) []byte {
 	buf, err := pb.Marshal(tx)
 	if err != nil {
--- types.go/transaction_test.go
+++ types.go/transaction_test.go
@@ -1,1 +1,37 @@
 package types
+
+import (
+	"testing"
+
+	"github.com/ranjankuldeep/DisBlocker/crypto"
+	"github.com/ranjankuldeep/DisBlocker/proto"
+	"github.com/ranjankuldeep/DisBlocker/util"
+)
+
+func TestNewTransaction(t *testing.T) {
+	fromPrivKey := crypto.GeneratePrivateKey()
+	fromAddress := fromPrivKey.Public().Address().Bytes()
+
+	toPrivKey := crypto.GeneratePrivateKey()
+	toAddress := toPrivKey.Public().Address().Bytes()
+
+	input := &proto.TxInput{
+		PrevHash:     util.RandomHash(),
+		PrevOutIndex: 0,
+		PublicKey:    fromPrivKey.Public().Bytes(),
+	}
+	output1 := &proto.TxOutput{
+		Amount:  5,
+		Address: toAddress,
+	}
+	output2 := &proto.TxOutput{
+		Amount:  95,
+		Address: fromAddress,
+	}
+	tx := &proto.Transaction{
+		Inputs:  []*proto.TxInput{input},
+		Outputs: []*proto.TxOutput{output1, output2},
+	}
+	sign := SignTransaction(fromPrivKey, tx)
+	input.Signature = sign.Bytes()
+}

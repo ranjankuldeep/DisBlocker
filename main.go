969c80ca27d223ca389e4aab69d055aeb3310750
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-06-27 18:33:16 +0530

    feat: grpc call working succesfully

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"time"
+
 	"github.com/ranjankuldeep/DisBlocker/logs"
 	"github.com/ranjankuldeep/DisBlocker/node"
 	"github.com/ranjankuldeep/DisBlocker/p2p"
+	"github.com/ranjankuldeep/DisBlocker/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -17,10 +23,35 @@ func main() {
 		Transport: tcp_transport,
 	}
 	nodeServer := node.NewNodeServer(*blockNode, &nodeOpts)
-
 	logs.Logger.Infof("Server Running on Port %s", tcpOptions.ListenAddr)
+
+	go func() {
+		for {
+			makeTransaction()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
 	if err := nodeServer.StartServer(); err != nil {
 		logs.Logger.Errorf("Error Starting NOde server %+v", err)
 		panic(err)
 	}
+}
+
+func makeTransaction() error {
+	client, err := grpc.NewClient(":3000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logs.Logger.Errorf("Error Making Transaction %+v", err)
+		return err
+	}
+	c := proto.NewNodeClient(client)
+	tx := &proto.Transaction{
+		Version: 1,
+	}
+	_, err = c.HandleTransaction(context.TODO(), tx)
+	if err != nil {
+		logs.Logger.Errorf("Error Making Transaction")
+		return err
+	}
+	return nil
 }
--- node/node.go
+++ node/node.go
@@ -3,7 +3,9 @@ package node
 import (
 	"context"
 
+	"github.com/ranjankuldeep/DisBlocker/logs"
 	"github.com/ranjankuldeep/DisBlocker/proto"
+	"google.golang.org/grpc/peer"
 )
 
 type Node struct {
@@ -15,5 +17,11 @@ func NewNode() *Node {
 }
 
 func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.None, error) {
+	perr, ok := peer.FromContext(ctx)
+	if !ok {
+		logs.Logger.Errorf("No peer exist")
+		return nil, nil
+	}
+	logs.Logger.Infof("Peer: %+v", perr)
 	return nil, nil
 }
